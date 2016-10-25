a15e85eb0bab44569f4b1663149ed75095461628
Niels Mulder <nc.mulder@outlook.com>
2016-10-25 15:39:54 +0200

    String tree replaced with path tree; better file type detection

--- ARCHIE_V1/src/archie_v1/ARCHIE.java
+++ ARCHIE_V1/src/archie_v1/ARCHIE.java
@@ -5,13 +5,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.net.URI;
+import static java.lang.System.out;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -20,7 +18,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.MutableTreeNode;
 import org.jdom2.*;
 import org.jdom2.output.XMLOutputter;
 
@@ -37,13 +34,12 @@ public class ARCHIE extends JPanel {
 
     public ARCHIE(Path path) {
         mainDir = path;
-        dirTree = new DefaultMutableTreeNode(path.getFileName());
+        dirTree = new DefaultMutableTreeNode(path);
         CreateNodes(path, dirTree);
         CreateDocument(dirTree);
         
-        
         XMLOutputter outputter = new XMLOutputter();
-        Path xmlfile = Paths.get("basic_xml.xml");
+        
         try {
             PrintWriter writer = new PrintWriter("basic_xml.xml");
             outputter.output(xml, writer);
@@ -53,18 +49,16 @@ public class ARCHIE extends JPanel {
             Logger.getLogger(ARCHIE.class.getName()).log(Level.SEVERE, "Writer not found", ex);
         }
 
+        //Filetree view
         fileTree = new JTree(dirTree);
-
         JScrollPane fileTreeView = new JScrollPane(fileTree);
-        //add(fileTreeView);
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         splitPane.setLeftComponent(fileTreeView);
         add(splitPane);
     }
 
     private void CreateNodes(Path path, DefaultMutableTreeNode tree) {
-        DefaultMutableTreeNode fileNode = new DefaultMutableTreeNode(path.getFileName());
-        //System.out.println("Added dir " + path.getFileName().toString());
+        DefaultMutableTreeNode fileNode = new DefaultMutableTreeNode(path);
         tree.add(fileNode);
         if (path.toFile().isDirectory()) {
             for (File file : path.toFile().listFiles()) {
@@ -87,23 +81,35 @@ public class ARCHIE extends JPanel {
     }
 
     private void CreateDocument(DefaultMutableTreeNode dirTree) {
-        Element root = new Element(dirTree.toString());
+        Element root = new Element("root");
         CreateElements(dirTree, root);
         
         xml = new Document(root);
-        System.out.println("xml doc: " + xml.toString());
+        out.println("xml doc: " + xml.toString());
     }
     
     private void CreateElements(DefaultMutableTreeNode dir, Element element){
-        if(dir.isLeaf()){
+        Path filePath = (Path)dir.getUserObject();
+        
+        if(filePath.toFile().isFile()){
             Element file = new Element("file");
-            file.setAttribute("name",dir.toString());
-            file.setAttribute("filetype", "unknown");
+            file.setAttribute("name", filePath.toString());
+            
+            String fileType = "unknown";
+            try {
+                String temp = Files.probeContentType(filePath);
+                if(temp!=null)
+                    fileType = temp;
+            } catch (IOException ex) {
+                Logger.getLogger(ARCHIE.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            file.setAttribute("filetype", fileType);
             element.addContent(file);
         } else {
             Element folder = new Element("folder");
-            folder.setAttribute("filecount", Integer.toString(dir.getChildCount()));
             folder.setAttribute("name", dir.toString());
+            folder.setAttribute("filecount", Integer.toString(dir.getChildCount()));
             for (Object file : Collections.list(dir.children())) {
                 CreateElements((DefaultMutableTreeNode)file, folder);
             }
