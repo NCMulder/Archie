d2036a6b1409c4bcc64fb649fac955addd671c23
Niels Mulder <nc.mulder@outlook.com>
2017-01-29 17:32:11 +0100

    Changed layout to GridLayout in Addables AddablePanels used GridBagLayouts before. This was a more suitable option for long fields, but general overview is better with gridlayout. Maybe a combination of the two is applicable. Also fixed a bug in exporting where old name formatting was used.

--- ARCHIE_V1/src/archie_v1/UI/AddablePanel.java
+++ ARCHIE_V1/src/archie_v1/UI/AddablePanel.java
@@ -9,6 +9,7 @@ import archie_v1.fileHelpers.FileHelper;
 import archie_v1.fileHelpers.MetadataKey;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -78,8 +79,7 @@ public class AddablePanel extends JPanel implements ActionListener {
     }
 
     private JComponent Values() {
-        JPanel gridPane = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc;
+        JPanel gridPane = new JPanel(new GridLayout(0,1));
 
         if (fileHelper.metadataMap.get(Values[0]) != null) {
             System.out.println("Addable " + Values[0] + " with value " + fileHelper.metadataMap.get(Values[0]) + " is being prepared.");
@@ -89,57 +89,46 @@ public class AddablePanel extends JPanel implements ActionListener {
             height = mainKeys.length;
 
             for (int i = 0; i < height; i++) {
-                JPanel singleValue = new JPanel(new GridBagLayout());
+                //This six is arbitrary, should be dynamically settable; however, we want all columns equally.
+                JPanel singleValue = new JPanel(new GridLayout(0, 6));
 
                 for (int j = 0; j < Values.length; j++) {
-                    String labelText = "";
-                    labelText = fileHelper.metadataMap.get(Values[j]).split(";")[i].trim();
-                    
-                    System.out.println("Label for " + Values[j] + " is set to " + labelText +", x index is " + j);
-                    
+                    String labelText = fileHelper.metadataMap.get(Values[j]).split(";")[i].trim();
                     JLabel label = new JLabel(labelText);
-                    gbc = new GridBagConstraints(
-                            j, 0, //GridX, GridY
-                            1, 1, //GridWidth, GridHeight
-                            xweight, 1, //WeightX, WeightY
-                            GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, //Anchor, Fill
-                            new Insets(2, 2, 2, 2), 0, 0); //Insets, IpadX, IpadY
-                    singleValue.add(label, gbc);
+                    singleValue.add(label);
                     //System.out.println("Labels size: " + (j * xweight) + "; Total size: " + (j * xweight + 0.2f));
+                }
+                
+                for (int j = Values.length; j < 5; j++) {
+                    JLabel label = new JLabel();
+                    singleValue.add(label);
                 }
 
                 JButton remove = new JButton("Remove");
                 remove.addActionListener(this);
-                gbc = new GridBagConstraints(
-                        5, 0, //GridX, GridY
-                        1, 1, //GridWidth, GridHeight
-                        1 - Values.length * xweight, 1, //WeightX, WeightY
-                        GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, //Anchor, Fill
-                        new Insets(2, 2, 2, 2), 0, 0); //Insets, IpadX, IpadY
-                singleValue.add(remove, gbc);
+                singleValue.add(remove);
 
-                gbc = new GridBagConstraints(
-                        0, i, //GridX, GridY
-                        5, 1, //GridWidth, GridHeight
-                        1, 1, //WeightX, WeightY
-                        GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, //Anchor, Fill
-                        new Insets(2, 2, 2, 2), 0, 0); //Insets, IpadX, IpadY
-                gridPane.add(singleValue, gbc);
+                gridPane.add(singleValue);
 
                 valuePanels.put(i, singleValue);
                 removeButtons.put(remove, i);
             }
         }
+        
+        //same arbitrary six here
+        JPanel addPanel = new JPanel(new GridLayout(0,6));
+        for (int i = 0; i < 5; i++) {
+            JLabel label = new JLabel();
+            addPanel.add(label);
+        }
 
         addItem = new JButton("Add " + singleTitle);
         addItem.addActionListener(this);
-        gbc = new GridBagConstraints(
-                4, height++, //GridX, GridY
-                1, 1, //GridWidth, GridHeight
-                .2f, 1, //WeightX, WeightY
-                GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, //Anchor, Fill
-                new Insets(2, 2, 2, 2), 0, 0); //Insets, IpadX, IpadY
-        gridPane.add(addItem, gbc);
+        addPanel.add(addItem);
+        
+        height++;
+        
+        gridPane.add(addPanel);
 
         JScrollPane scrollPane = new JScrollPane(gridPane);
         scrollPane.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
--- ARCHIE_V1/src/archie_v1/outputFormats/outputIslandora.java
+++ ARCHIE_V1/src/archie_v1/outputFormats/outputIslandora.java
@@ -14,18 +14,18 @@ import org.jdom2.Element;
 import org.jdom2.Namespace;
 
 public class outputIslandora extends outputAbstract {
-
+    
     Namespace rootNamespace;
-
+    
     public outputIslandora() {
         super();
     }
-
+    
     @Override
     public void Save(String destination, ArrayList<FileHelper> files) throws IOException {
         Zipper zipper = new Zipper();
         HashMap<Path, Document> documentMap = new HashMap();
-
+        
         for (FileHelper fileHelper : files) {
             Document doc = FileToDocument(fileHelper);
             documentMap.put(fileHelper.filePath, doc);
@@ -34,10 +34,10 @@ public class outputIslandora extends outputAbstract {
         //Instruct the zipper to save the generated .xml-documents and their associated files to a zip.
         zipper.SaveAsZip(destination, documentMap);
     }
-
+    
     public Document FileToDocument(FileHelper fileHelper) {
         Element root = getRootElement();
-
+        
         for (Element element : getMODSElements(fileHelper)) {
             if (element != null) {
                 root.addContent(element);
@@ -54,7 +54,7 @@ public class outputIslandora extends outputAbstract {
 
         return fileXML;
     }
-
+    
     private Element getRootElement() {
         rootNamespace = Namespace.getNamespace("http://www.loc.gov/mods/v3");
         Namespace ns2 = Namespace.getNamespace("xsi", "https://www.w3.org/2001/XMLSchema-instance");
@@ -64,7 +64,7 @@ public class outputIslandora extends outputAbstract {
         modsXML.addNamespaceDeclaration(ns2);
         modsXML.addNamespaceDeclaration(ns3);
         modsXML.setAttribute("version", "3.6");
-
+        
         return modsXML;
     }
 
@@ -98,40 +98,44 @@ public class outputIslandora extends outputAbstract {
         } else if (!"".equals(fullName)) {
             familyName = fullName;
         }
-
+        
         Element namePartGiven = new Element("namePart", rootNamespace);
         namePartGiven.setAttribute("type", "given");
         namePartGiven.setText(givenName);
-
+        
         Element namePartFamily = new Element("namePart", rootNamespace);
         namePartFamily.setAttribute("type", "family");
         namePartFamily.setText(familyName);
-
+        
         Element[] names = new Element[2];
         names[0] = namePartFamily;
         names[1] = namePartGiven;
-
+        
         return names;
     }
-
+    
     public LinkedList<Element> getMODSElements(FileHelper fileHelper) {
         LinkedList<Element> elementList = new LinkedList();
 
         //elementList.add(getTitle(fileHelper));
-        elementList.add(getIdentifier(fileHelper));
-        elementList.add(getCreator(fileHelper));
-        elementList.addAll(Arrays.asList(getContributors(fileHelper)));
+        //elementList.add(getIdentifier(fileHelper));
+        Element[] creators = getCreator(fileHelper);
+        if(creators!=null)
+            elementList.addAll(Arrays.asList(creators));
+        Element[] contributors = getContributors(fileHelper);
+        if(contributors!=null)
+            elementList.addAll(Arrays.asList(contributors));
         //elementList.add(getRightsHolder(fileHelper));
         //elementList.add(getRelatedItem(fileHelper));
-        elementList.add(getRelatedDataSet(fileHelper));
-        elementList.add(getSubject(fileHelper));
-        elementList.add(getAbstract(fileHelper));
-        elementList.add(getOriginInfo(fileHelper));
-        elementList.add(getTypeOfResource());
-        elementList.add(getLanguage(fileHelper));
-        elementList.add(getAccessLevel(fileHelper));
-        elementList.add(getFileSize(fileHelper));
-
+        //elementList.add(getRelatedDataSet(fileHelper));
+        //elementList.add(getSubject(fileHelper));
+        //elementList.add(getAbstract(fileHelper));
+        //elementList.add(getOriginInfo(fileHelper));
+        //elementList.add(getTypeOfResource());
+        //elementList.add(getLanguage(fileHelper));
+        //elementList.add(getAccessLevel(fileHelper));
+        //elementList.add(getFileSize(fileHelper));
+        
         return elementList;
     }
 
@@ -142,108 +146,120 @@ public class outputIslandora extends outputAbstract {
         Element title = new Element("title", rootNamespace);
         title.setText(fileHelper.metadataMap.get(MetadataKey.FileUnits));
         titleInfo.addContent(title);
-
+        
         return titleInfo;
     }
-
+    
     public Element getIdentifier(FileHelper fileHelper) {
         Element identifier = new Element("identifier", rootNamespace);
         identifier.setAttribute("type", "doi");
         identifier.setText(fileHelper.metadataMap.get(MetadataKey.Identifier));
         return identifier;
     }
-
-    public Element getCreator(FileHelper fileHelper) {
-        Element[] names = nameHandler(fileHelper.metadataMap.get(MetadataKey.CreatorGivenName), fileHelper);
-
-        Element name = new Element("name", rootNamespace);
-        name.setAttribute("type", "personal");
-
-        //how do we implement role terms?
-        Element role = new Element("role", rootNamespace);
-        Element roleTerm = new Element("roleTerm", rootNamespace);
-        roleTerm.setAttribute("type", "text");
-        roleTerm.setAttribute("authority", "unknown");
-        roleTerm.setText("creator");
-        role.addContent(roleTerm);
-        name.addContent(role);
-
-        String TOA = fileHelper.metadataMap.get(MetadataKey.CreatorTOA);
-        if (!"None".equals(TOA)) {
-            Element namePartTOA = new Element("namePart", rootNamespace);
-            namePartTOA.setAttribute("type", "termsOfAdress");
-            namePartTOA.setText(TOA);
-        }
-
-        name.addContent(names[1]);
-        name.addContent(names[0]);
-
-        //How do we implement name identifiers?
-        Element nameIdentifier = new Element("nameIdentifier", rootNamespace);
-        nameIdentifier.setAttribute("type", "DAI");
-        nameIdentifier.setText(fileHelper.metadataMap.get(MetadataKey.CreatorIdentifier));
-        name.addContent(nameIdentifier);
-
-        Element affiliation = new Element("affiliation", rootNamespace);
-        affiliation.setText(fileHelper.metadataMap.get(MetadataKey.CreatorAffiliation));
-        name.addContent(affiliation);
-
-        return name;
-    }
-
-    public Element[] getContributors(FileHelper fileHelper) {
-        String[] contributorTOA = fileHelper.metadataMap.get(MetadataKey.ContributorTOA).split(";");
-        String[] contributorNames = fileHelper.metadataMap.get(MetadataKey.ContributorGivenName).split(";");
-        String[] contributorIdentifier = fileHelper.metadataMap.get(MetadataKey.ContributorIdentifier).split(";");
-        String[] contributorAffiliation = fileHelper.metadataMap.get(MetadataKey.ContributorAffiliation).split(";");
-        Element[] contributors = new Element[contributorNames.length];
-
-        for (int i = 0; i < contributorNames.length; i++) {
-            Element[] names = nameHandler(contributorNames[i], fileHelper);
-
+    
+    public Element[] getCreator(FileHelper fileHelper) {
+        String creatorNames = fileHelper.metadataMap.get(MetadataKey.CreatorFamilyName);
+        if(creatorNames==null)
+            return null;
+        int creatorCount = creatorNames.split(";").length;
+        Element[] creators = new Element[creatorCount];
+        for (int i = 0; i < creatorCount; i++) {
             Element name = new Element("name", rootNamespace);
             name.setAttribute("type", "personal");
-
-            //how do we implement role terms?
+            
             Element role = new Element("role", rootNamespace);
             Element roleTerm = new Element("roleTerm", rootNamespace);
             roleTerm.setAttribute("type", "text");
-            roleTerm.setAttribute("authority", "unknown");
-            roleTerm.setText("contributor");
+            roleTerm.setText("creator");
             role.addContent(roleTerm);
             name.addContent(role);
-
-            //redo this TODO
-            if(i < contributorTOA.length){
-            String TOA = contributorTOA[i].replace(" ", "");
-            if (!"".equals(TOA)&&!"None".equals(TOA)) {
+            
+            String TOA = fileHelper.metadataMap.get(MetadataKey.CreatorTOA).split(";")[i];
+            if (TOA != null && !"None".equals(TOA)) {
                 Element namePartTOA = new Element("namePart", rootNamespace);
                 namePartTOA.setAttribute("type", "termsOfAdress");
                 namePartTOA.setText(TOA);
                 name.addContent(namePartTOA);
             }
-            }
+            
+            Element namePartGiven = new Element("namePart", rootNamespace);
+            namePartGiven.setAttribute("type", "given");
+            namePartGiven.setText(fileHelper.metadataMap.get(MetadataKey.CreatorGivenName).split(";")[i]);
+            
+            Element namePartFamily = new Element("namePart", rootNamespace);
+            namePartFamily.setAttribute("type", "family");
+            namePartFamily.setText(fileHelper.metadataMap.get(MetadataKey.CreatorFamilyName).split(";")[i]);
+            
+            name.addContent(namePartGiven);
+            name.addContent(namePartFamily);
 
-            name.addContent(names[1]);
-            name.addContent(names[0]);
+            //How do we implement name identifiers?
+            Element nameIdentifier = new Element("nameIdentifier", rootNamespace);
+            nameIdentifier.setAttribute("type", "DAI");
+            nameIdentifier.setText(fileHelper.metadataMap.get(MetadataKey.CreatorIdentifier).split(";")[i]);
+            name.addContent(nameIdentifier);
+            
+            Element affiliation = new Element("affiliation", rootNamespace);
+            affiliation.setText(fileHelper.metadataMap.get(MetadataKey.CreatorAffiliation).split(";")[i]);
+            name.addContent(affiliation);
+            
+            creators[i] = name;
+        }
+        
+        return creators;
+    }
+    
+    public Element[] getContributors(FileHelper fileHelper) {
+        String contributorNames = fileHelper.metadataMap.get(MetadataKey.ContributorFamilyName);
+        if(contributorNames==null)
+            return null;
+        int contributorCount = contributorNames.split(";").length;
+        Element[] contributors = new Element[contributorCount];
+        
+        for (int i = 0; i < contributorCount; i++) {
+            Element name = new Element("name", rootNamespace);
+            name.setAttribute("type", "personal");
+
+            Element role = new Element("role", rootNamespace);
+            Element roleTerm = new Element("roleTerm", rootNamespace);
+            roleTerm.setAttribute("type", "text");
+            roleTerm.setText("contributor");
+            role.addContent(roleTerm);
+            name.addContent(role);
 
             //redo this TODO
-            //How do we implement name identifiers?
-            if(i<contributorIdentifier.length){
-            Element nameIdentifier = new Element("nameIdentifier", rootNamespace);
-            nameIdentifier.setText(contributorIdentifier[i]);
-            name.addContent(nameIdentifier);
+            String TOA = fileHelper.metadataMap.get(MetadataKey.ContributorTOA).split(";")[i];
+            if (TOA != null && !"None".equals(TOA)) {
+                Element namePartTOA = new Element("namePart", rootNamespace);
+                namePartTOA.setAttribute("type", "termsOfAdress");
+                namePartTOA.setText(TOA);
+                name.addContent(namePartTOA);
             }
+            
+            Element namePartGiven = new Element("namePart", rootNamespace);
+            namePartGiven.setAttribute("type", "given");
+            namePartGiven.setText(fileHelper.metadataMap.get(MetadataKey.ContributorGivenName).split(";")[i]);
+            
+            Element namePartFamily = new Element("namePart", rootNamespace);
+            namePartFamily.setAttribute("type", "family");
+            namePartFamily.setText(fileHelper.metadataMap.get(MetadataKey.ContributorFamilyName).split(";")[i]);
+            
+            name.addContent(namePartGiven);
+            name.addContent(namePartFamily);
 
-            if(i<contributorAffiliation.length){
+            //How do we implement name identifiers?
+            Element nameIdentifier = new Element("nameIdentifier", rootNamespace);
+            nameIdentifier.setAttribute("type", "DAI");
+            nameIdentifier.setText(fileHelper.metadataMap.get(MetadataKey.ContributorIdentifier).split(";")[i]);
+            name.addContent(nameIdentifier);
+            
             Element affiliation = new Element("affiliation", rootNamespace);
-            affiliation.setText(contributorAffiliation[i]);
+            affiliation.setText(fileHelper.metadataMap.get(MetadataKey.ContributorAffiliation).split(";")[i]);
             name.addContent(affiliation);
-            }
             
             contributors[i] = name;
         }
-
+        
         return contributors;
     }
 
@@ -259,7 +275,7 @@ public class outputIslandora extends outputAbstract {
         //almost always manually set
         return null;
     }
-
+    
     public Element getRelatedDataSet(FileHelper fileHelper) {
         Element relatedItem = new Element("relatedItem", rootNamespace);
         
@@ -272,23 +288,22 @@ public class outputIslandora extends outputAbstract {
         Element relatedLocation = new Element("location", rootNamespace);
         Element relatedURL = new Element("url", rootNamespace);
         //is this really necessary?
-        relatedURL.setAttribute("usage","primary");
+        relatedURL.setAttribute("usage", "primary");
         relatedURL.setText(fileHelper.metadataMap.get(MetadataKey.RelatedDatasetLocation));
         relatedLocation.addContent(relatedURL);
         relatedItem.addContent(relatedLocation);
         
         return relatedItem;
     }
-
+    
     public Element getSubject(FileHelper fileHelper) {
         Element subject = new Element("subject", rootNamespace);
         
         Element topic = new Element("topic", rootNamespace);
         topic.setText(fileHelper.metadataMap.get(MetadataKey.Subject));
         subject.addContent(topic);
-        
+
         //Not for all files...
-        
 //        String[] temps = fileHelper.metadataMap.get(MetadataKey.TemporalCoverage).split(" - ");
 //        
 //        Element temporalStart = new Element("temporal", rootNamespace);
@@ -302,10 +317,9 @@ public class outputIslandora extends outputAbstract {
 //            temporalEnd.setText(temps[1].replace(" ",""));
 //            subject.addContent(temporalEnd);
 //        }
-        
         String[] coords = fileHelper.metadataMap.get(MetadataKey.SpatialCoverage).split(";");
         Element cartographics = new Element("cartographics", rootNamespace);
-        for(String coord : coords){
+        for (String coord : coords) {
             Element coordinates = new Element("coordinates", rootNamespace);
             coordinates.setText(coord.replace(" ", ""));
             cartographics.addContent(coordinates);
@@ -314,14 +328,14 @@ public class outputIslandora extends outputAbstract {
         
         return subject;
     }
-
+    
     public Element getAbstract(FileHelper fileHelper) {
         Element abstractEl = new Element("abstract", rootNamespace);
         abstractEl.setText(fileHelper.metadataMap.get(MetadataKey.Description));
         
         return abstractEl;
     }
-
+    
     public Element getOriginInfo(FileHelper fileHelper) {
         Element originInfo = new Element("originInfo", rootNamespace);
         Element publisher = new Element("publisher", rootNamespace);
@@ -330,7 +344,7 @@ public class outputIslandora extends outputAbstract {
         Element creationDate = new Element("dateCreated", rootNamespace);
         creationDate.setText(fileHelper.metadataMap.get(MetadataKey.DateCreated));
         originInfo.addContent(creationDate);
-
+        
         return originInfo;
     }
 
@@ -347,18 +361,18 @@ public class outputIslandora extends outputAbstract {
     public Element getTotalSize(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getLanguage(FileHelper fileHelper) {
         Element language = new Element("language", rootNamespace);
         Element languageTerm = new Element("languageTerm", rootNamespace);
         //todo: redo languages from text to code
-        languageTerm.setAttribute("type","text");
+        languageTerm.setAttribute("type", "text");
         languageTerm.setText(fileHelper.metadataMap.get(MetadataKey.Language));
         language.addContent(languageTerm);
         
         return language;
     }
-
+    
     public Element getAccessLevel(FileHelper fileHelper) {
         Element accessCondition = new Element("accessCondition", rootNamespace);
         accessCondition.setText(fileHelper.metadataMap.get(MetadataKey.AccessLevel));
@@ -370,43 +384,43 @@ public class outputIslandora extends outputAbstract {
     public Element getCollector(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getDescription(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getPurpose(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getCollection(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getUnits(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getAppreciation(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getSource(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getCitation(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getNotes(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getCoordinates(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getContentType(FileHelper fileHelper) {
         return null;
     }
@@ -415,22 +429,22 @@ public class outputIslandora extends outputAbstract {
     public Element getForm(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getFileSize(FileHelper fileHelper) {
         System.out.println(fileHelper.filePath.toFile().length());
         Element fileSize = new Element("FileSize");
         fileSize.setText("boe");
         return fileSize;
     }
-
+    
     public Element getPhysicalForm(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getSourceType(FileHelper fileHelper) {
         return null;
     }
-
+    
     public Element getPhysicalDescription(FileHelper fileHelper) {
         return null;
     }
